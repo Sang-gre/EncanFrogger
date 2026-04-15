532570a967074ba076c7a3d2890d05b838d195da
ming12230 <jubiemyrsilvio12@gmail.com>
2026-04-15 21:59:22 +0800

    started with the data management system
    d ko pa tapos, tom nalng or when ko feel

--- src/data/SaveState.java
+++ src/data/SaveState.java
@@ -1,8 +1,28 @@
 package data;
 
+import gameobjects.PlayerType;
+
 public class SaveState {
+    private int playerX;
+    private int playerY;
+    private int score;
     private int level;
-    private int coins;
     private int lives;
-    private String playerType;
+    private PlayerType playerType;
+
+    public SaveState(int x, int y, int score, int level, int lives, PlayerType playerType) {
+        this.playerX = x;
+        this.playerY = y;
+        this.score = score;
+        this.level = level;
+        this.lives = lives;
+        this.playerType = playerType;
+    }
+
+    public int getPlayerX() { return playerX; }
+    public int getPlayerY() { return playerY; }
+    public int getScore() { return score; }
+    public int getLevel() { return level; }
+    public int getLives() { return lives; }
+    public PlayerType getPlayerType() { return playerType; }
 }
---
+++ src/gameobjects/PlayerType.java
@@ -0,0 +1,3 @@
+package gameobjects;
+
+public enum PlayerType { PAOPAO, TERRA, FLAMARA, ADAMUS, DEIA}
