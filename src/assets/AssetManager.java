39ae0d0736c27d1104e7f3880405a1daec4669f5
minchunie <jjparcero@up.edu.ph>
2026-04-22 11:10:29 +0800

    manage player sprites
    Refactor AssetManager to load player sprites dynamically from specified paths.

--- src/assets/AssetManager.java
+++ src/assets/AssetManager.java
@@ -1,16 +1,42 @@
 package assets;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import gameobjects.PlayerType;
+import level.Direction;
+// ichachange pa kasi dapat hiwalay-hiwalay ang png for fron, back, etc.
 public class AssetManager {
-    /* public static final Image PAOPAO_FRONT =
-            new ImageIcon("assets/paopao/front.png").getImage();
+/*
+    private static final Map<PlayerType, Map<Direction, Image>> playerSprites = new HashMap<>();
 
-    public static final Image PAOPAO_BACK =
-            new ImageIcon("assets/paopao/back.png").getImage();
+    static {
+        loadPlayerSprites(PlayerType.PAOPAO, "assets/spritesheets/paopao/");
+        loadPlayerSprites(PlayerType.DEIA, "assets/spritesheets/deia/");
+        loadPlayerSprites(PlayerType.FLAMARA, "assets/spritesheets/flammara/");
+        loadPlayerSprites(PlayerType.TERRA, "assets/spritesheets/terra/");
+        loadPlayerSprites(PlayerType.ADAMUS, "assets/spritesheets/adamus/");
+    }
 
-    public static final Image PAOPAO_LEFT =
-            new ImageIcon("assets/paopao/left.png").getImage();
+    private static void loadPlayerSprites(PlayerType type, String path) {
+        Map<Direction, Image> sprites = new HashMap<>();
 
-    public static final Image PAOPAO_RIGHT =
-            new ImageIcon("assets/paopao/right.png").getImage();
+        sprites.put(Direction.UP, new ImageIcon(path + "back.png").getImage());
+        sprites.put(Direction.DOWN, new ImageIcon(path + "front.png").getImage());
+        sprites.put(Direction.LEFT, new ImageIcon(path + "left.png").getImage());
+        sprites.put(Direction.RIGHT, new ImageIcon(path + "right.png").getImage());
+
+        playerSprites.put(type, sprites);
+    }
+
+    public static Image getPlayerSprite(PlayerType type, Direction direction) {
+        Map<Direction, Image> sprites = playerSprites.get(type);
+
+        if (sprites == null) return null;
+
+        return sprites.getOrDefault(direction, sprites.get(Direction.DOWN));
+    }
     */
 }
