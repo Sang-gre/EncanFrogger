1600d41424e3254fecda3fc0d7f88202b21e2210
minchunie <jjparcero@up.edu.ph>
2026-04-22 11:03:13 +0800

    Comment out asset loading logic in AssetManager
    Commented out the asset loading logic for images.

--- src/assets/AssetManager.java
+++ src/assets/AssetManager.java
@@ -1,5 +1,16 @@
 package assets;
 
 public class AssetManager {
-    // TODO: Implement asset management logic, such as loading and caching assets (e.g., images, sounds, etc.)
+    /* public static final Image PAOPAO_FRONT =
+            new ImageIcon("assets/paopao/front.png").getImage();
+
+    public static final Image PAOPAO_BACK =
+            new ImageIcon("assets/paopao/back.png").getImage();
+
+    public static final Image PAOPAO_LEFT =
+            new ImageIcon("assets/paopao/left.png").getImage();
+
+    public static final Image PAOPAO_RIGHT =
+            new ImageIcon("assets/paopao/right.png").getImage();
+    */
 }
